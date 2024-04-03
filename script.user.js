b397fe6bf98a150bc36f1631be75b42355cc077f
HoJeong Go <seia@outlook.kr>
2024-04-03 21:11:54 +0900

    fix: typescript syntax

--- script.user.js
+++ script.user.js
@@ -19,7 +19,7 @@ async function documentReady () {
     return null
   }
 
-  return new Promise() < null > (resolve => {
+  return new Promise(resolve => {
     document.addEventListener('readystatechange', () => {
       resolve(null)
     })
